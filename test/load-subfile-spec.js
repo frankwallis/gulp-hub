29ce74a32e0afef0a2258854b31d7c3076cf3e69
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-07-29 20:56:07 -0700

    Implement validation for hub object

--- test/load-subfile-spec.js
+++ test/load-subfile-spec.js
@@ -4,6 +4,9 @@ var sinon   = require( 'sinon' );
 var pequire = require( 'proxyquire' );
 
 var HAPPY_PROXY_DEPS = {
+    gulp: {
+        Gulp: _.noop
+    },
     './hub-util': {
         isValidHubFile: function (){ return true }
     },
@@ -19,10 +22,19 @@ describe.only( 'load-subfile', function () {
     it( 'errors if subfile is not a valid Gulp Hub file', function () {
         var loadSubfile = getLoad( {
             './hub-util': {
-                isValidHubFile: function (){ return false }
+                isValidHubFile: function () { return false }
             }
         } );
         loadSubfile.should.throw( '`subfile` must be a valid Gulp Hub file object.' );
     } );
 
+    it( 'errors if the task registry is not an object', function () {
+        var loadSubfile = getLoad();
+        var INVALID_VALUES = [ '', 0, 1, true, false, [], null, undefined ];
+        INVALID_VALUES.forEach( function ( testTasksParam ) {
+            loadSubfile.bind( null, null, testTasksParam ).should.throw( '`task` must be an object' );
+        } );
+    } );
+
+    it( 'loads the specified file' );
 } );
