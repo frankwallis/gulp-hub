22cb46c44604d70a724115861a235ad8c0b38913
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-07-29 18:18:00 -0700

    Add spec file for load-subfile module

---
+++ test/load-subfile-spec.js
@@ -0,0 +1,11 @@
+var should  = require( 'should' );
+var sinon   = require( 'sinon' );
+var pequire = require( 'proxyquire' );
+
+describe.only( 'load-subfile', function () {
+
+    it( 'ensures the subfile is a gulp-hub file object' );
+
+    it( 'ensures the task registry is an object' );
+    
+} );
