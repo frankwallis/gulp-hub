146ae40274ae9cef047933821989e80783986e3f
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-07-30 20:02:28 -0700

    Add add-task spec file and test stubs

---
+++ test/add-task-spec.js
@@ -0,0 +1,11 @@
+var should  = require( 'should' );
+var sinon   = require( 'sinon' );
+var pequire = require( 'proxyquire' );
+
+describe( 'add-task', function () {
+
+    it( 'adds each tasks\'s subtasks and their parameters to gulp' );
+
+    it( 'creates a master task to run all subtasks in sequence' );
+
+} );
