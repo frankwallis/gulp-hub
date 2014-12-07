71bf4dc90737f421e262ff4bcaab7fb88401cb6a
simon <ihmig@kaliber5.de>
2014-12-07 22:00:00 +0100

    Ability to extend existing tasks in the main gulpfile with subtasks: existing task is preserved, subtasks with same name are added as dependencies

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -4,5 +4,13 @@
 	> gulp watch
 	> gulp compile
 */
-var hub = require('../lib/index.js');
+var hub = require('../lib/index.js')
+    gulp = require('gulp');
+
+// main compile task, compile subtasks will be added as dependencies to this task
+gulp.task('compile', function() {
+    console.log('compiling: main task')
+});
+
+// call hub after all tasks in the main gulpfile have been added
 hub(['project1/gulpfile.js', 'proj*/gulpfile.js', 'project1/**/gulpfile.js', '!node_modules/**']);
